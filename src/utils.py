18e69baa0dd36c3c6f9afe4b69eaca0d6bcf44f2
esmrzv <esmurziev.1999@mail.ru>
2024-03-16 11:58:38 +0300

    add classes

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+class Category:
+    all_quantity_category = 0
+    all_unique_product = 0
+    name: str
+    description: str
+    products: list
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        self.all_quantity_category = 1
+
+        Category.all_unique_product += 1
+
+
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
