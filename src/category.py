8160d73fc1ad900dbaffaa618270d790d6ec57e1
esmrzv <esmurziev.1999@mail.ru>
2024-03-17 14:26:44 +0300

    renamed files

--- src/category.py
+++ src/category.py
@@ -15,14 +15,4 @@ class Category:
         Category.all_unique_product += len(self.products)
 
 
-class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
 
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
---
+++ tests/test_product.py
@@ -0,0 +1,12 @@
+import pytest
+from src.product import Product
+@pytest.fixture
+def correct_init_products():
+    return Product("Iphone 15", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+def test_init_product(correct_init_products):
+    assert correct_init_products.name == "Iphone 15"
+    assert correct_init_products.description == "256GB, Серый цвет, 200MP камера"
+    assert correct_init_products.price == 180000.0
+    assert correct_init_products.quantity == 5
