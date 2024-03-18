a6f8981bc21fb302e3960100c7bf3d222d88ad67
esmrzv <esmurziev.1999@mail.ru>
2024-03-18 16:40:05 +0300

    add changes

--- src/category.py
+++ src/category.py
@@ -1,18 +1,33 @@
+
+
 class Category:
     all_quantity_category = 0
     all_unique_product = 0
     name: str
     description: str
-    products: list
+    __products: list
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.all_quantity_category += 1
 
-        Category.all_unique_product += len(self.products)
+        Category.all_unique_product += len(self.__products)
 
+    @property
+    def products(self):
+        return self.__products
+
+    def add_product(self, product):
+        self.__products.append(product)
+
+    @property
+    def __str__(self):
+        products_list = []
+        for product in self.__products:
+            products_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity}шт")
+        return products_list
 
 
--- src/product.py
+++ src/product.py
@@ -3,9 +3,20 @@ class Product:
     description: str
     price: float
     quantity: int
+    products = []
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def create_product(cls, name, description, price, quantity):
+        new_product = cls(name, description, price, quantity)
+        cls.products.append(new_product)
+        return new_product
+
+    def __repr__(self):
+        """Хранит в списках шаблон по заданию 13.2 - Задача 2"""
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
